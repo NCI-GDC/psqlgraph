030a1a05499bcc7c29568a26bc62143b61d79330
Joshua Miller <jshuasmiller@gmail.com>
2014-12-08 21:47:23 -0600

    node export now creates index on id for each type of node label

--- psqlgraph/psqlgraph2neo4j.py
+++ psqlgraph/psqlgraph2neo4j.py
@@ -8,6 +8,9 @@ class PsqlGraph2Neo4j(object):
     def __init__(self):
         self.psqlgraphDriver = None
         self.neo4jDriver = None
+        self.indexed_keys = [
+            'id'
+        ]
 
     def connect_to_psql(self, host, user, password, database):
         self.psqlgraphDriver = psqlgraph.PsqlGraphDriver(
@@ -36,7 +39,30 @@ class PsqlGraph2Neo4j(object):
             node.properties['id'] = node.node_id
             self.try_parse_doc(node.properties)
 
+    def get_distinct_labels(self):
+        conn = self.psqlgraphDriver.engine.connect()
+        conn.execute('commit')
+        labels = conn.execute('select distinct label from nodes').fetchall()
+        conn.close()
+        return [l[0] for l in labels]
+
+    def create_indexes(self):
+        labels = self.get_distinct_labels()
+        for label in labels:
+            for key in self.indexed_keys:
+                self.neo4jDriver.schema.create_index(label, key)
+
     def export(self):
+        try:
+            self.create_indexes()
+        except Exception, msg:
+            print "unable to create indexes", str(msg)
+
         for node in self.psqlgraphDriver.get_nodes():
             self.convert_node(node)
             self.neo4jDriver.create(py2neo.Node(node.label, **node.properties))
+
+        for edge in self.psqlgraphDriver.get_edges():
+            self.neo4jDriver.cypher.execute(
+                'CREATE'
+            )

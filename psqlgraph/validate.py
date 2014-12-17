839aa95f37d9fbb3e90fcd78760a5273c17bb4c9
James Porter <porterjamesj@gmail.com>
2014-12-17 11:44:06 -0600

    add validation for no unexpected props

--- psqlgraph/validate.py
+++ psqlgraph/validate.py
@@ -23,6 +23,24 @@ class PsqlNodeValidator(object):
         return True
 
 
+def validate_no_unexpected_props(schema, datum):
+    """The default avro validator does not validate the absence of
+    unexpected props, so we do this as a separate step."""
+    if schema.type in ['union', 'error_union']:
+        return any((validate_no_unexpected_props(s, datum) for s in schema.schemas))
+    if schema.type in ['record', 'error', 'request']:
+        if not isinstance(datum, dict):
+            return False
+        field_names = [f.name for f in schema.fields]
+        if any((k not in field_names for k in datum.keys())):
+            return False
+        else:
+            return all((validate_no_unexpected_props(f.type, datum[f.name])
+                        for f in schema.fields))
+    else:
+        return True
+
+
 class AvroNodeValidator(PsqlNodeValidator):
     def __init__(self, schema):
         self.schema = schema
@@ -36,4 +54,5 @@ class AvroNodeValidator(PsqlNodeValidator):
 
     def validate(self, node, *args, **kwargs):
         node_as_dict = self.munge_node_into_dict(node)
-        return avro_validate(self.schema, node_as_dict)
+        return (avro_validate(self.schema, node_as_dict)
+                and validate_no_unexpected_props(self.schema, node_as_dict))

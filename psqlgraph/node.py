98ae1d396400e2a10d5d07eb449e680d4900488d
Joshua Miller <jshuasmiller@gmail.com>
2015-04-12 20:10:34 -0500

    updating subclass property handling

--- psqlgraph/node.py
+++ psqlgraph/node.py
@@ -5,13 +5,19 @@ from sqlalchemy.orm import object_session
 from datetime import datetime
 import copy
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import event
 
 
 Base = declarative_base()
 
 
+def node_load_listener(target, context):
+    if target.is_inherited():
+        target._load_properties()
+
+
 class Node(Base):
-    _properties = []
+
     __tablename__ = '_nodes'
     __table_args__ = (UniqueConstraint('_id', name='_node_id_uc'),)
 
@@ -49,18 +55,53 @@ class Node(Base):
 
     def __init__(self, _id=_id, _label=None, acl=[],
                  system_annotations={}, properties={}):
-
-        assert hasattr(self, '_properties'), \
-            'Please define _properties attr in your model'
-
-        self._label = self.__class__.__name__.lower()
+        self.__class__ = self.get_subclass(_label)
+        self._label = _label if _label else self.__class__.__name__
         self._sysan = system_annotations
         self._id = _id
         self._acl = acl
         self._label = _label
+        self.set_properties(properties)
+        if self.is_inherited():
+            self._load_properties()
+
+    def get_subclass(self, _label):
+        if self.is_inherited():
+            return type(self)
+        for c in type(self).__subclasses__():
+            c_label = getattr(c, '__mapper_args__', {}).get(
+                'polymorphic_identity', None)
+            if c_label == _label:
+                return c
+        raise KeyError('Node has no subclass {}'.format(_label))
+
+    def _load_properties(self):
+        assert hasattr(self, '_properties'),\
+            'Please add _properties attribute to model {}'.format(
+                type(self).__name__)
+        self.__safe_set_attr__('properties', {
+            k: getattr(self, k) for k in self._properties})
+
+    def __safe_set_attr__(self, name, value):
+        self.__dict__[name] = value
+
+    def __setattr__(self, name, value):
+        self.__safe_set_attr__(name, value)
+        self._load_properties()
+
+    @classmethod
+    def get_subclass_table_names(_label):
+        return [s.__tablename__ for s in Node.__subclasses__()]
+
+    def set_properties(self, properties):
         for key, value in properties.items():
             setattr(self, key, value)
-        self.properties = {k: getattr(self, k) for k in self._properties}
+
+    def get_name(self):
+        return type(self).__name__
+
+    def is_inherited(self):
+        return not type(self) == Node
 
     def get_session(self):
         return object_session(self)
@@ -99,3 +140,5 @@ class Node(Base):
 
 class PsqlNode(Node):
     pass
+
+event.listen(Node, 'load', node_load_listener, propagate=True)

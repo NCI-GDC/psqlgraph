166f81f00c3b2980e6c4722f7b3d311644754bf1
Joshua Miller <jshuasmiller@gmail.com>
2015-04-21 18:12:52 +0000

    peg to sqlalchemy 9.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=["psqlgraph"],
     install_requires=[
         'psycopg2==2.5.4',
-        'sqlalchemy==0.9.8',
+        'sqlalchemy==0.9.9',
         'py2neo==2.0.1',
         'progressbar',
         'avro==1.7.7',

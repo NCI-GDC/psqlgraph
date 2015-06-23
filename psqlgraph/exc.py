84e9286f1c2edd75cdd264a3549d2bbc6fc7b08e
MurphyMarkW <murphy.mark.w+github@gmail.com>
2015-06-23 21:01:57 +0000

    Adds base error class for PSQLGraph and SessionClosedError.

--- psqlgraph/exc.py
+++ psqlgraph/exc.py
@@ -1,18 +1,30 @@
-class QueryError(Exception):
+class PSQLGraphError(Exception):
+    """Base class for PostgreSQL Graph Errors.
+    """
     pass
 
 
-class ProgrammingError(Exception):
+class QueryError(PSQLGraphError):
     pass
 
 
-class NodeCreationError(Exception):
+class ProgrammingError(PSQLGraphError):
     pass
 
 
-class EdgeCreationError(Exception):
+class NodeCreationError(PSQLGraphError):
     pass
 
 
-class ValidationError(Exception):
+class EdgeCreationError(PSQLGraphError):
+    pass
+
+
+class ValidationError(PSQLGraphError):
+    pass
+
+
+class SessionClosedError(PSQLGraphError):
+    """An operation was requested from a closed session.
+    """
     pass

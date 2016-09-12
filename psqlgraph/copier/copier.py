78390b96b0944b6374f5aa3db3fc029d527a864b
Joshua Miller <jsmiller@uchicago.edu>
2016-09-12 13:19:27 -0500

    feat(copier): copy instantiated nodes, edges

--- psqlgraph/copier/copier.py
+++ psqlgraph/copier/copier.py
@@ -68,3 +68,43 @@ def copy_edges(destination_db, source_query):
 
     for chunk in chunk_edge_tuple_query(source_query):
         upsert_edges(destination_db, entity_type, chunk)
+
+
+def eagerly_reload_nodes(db, nodes):
+    """Reload nodes. This is a tradeoff in performance to reload
+    all of the nodes, with the idea that the edges for all of
+    them will be eagerly loaded and getting all of the edges
+    will be faster.
+
+    """
+
+    ids = [n.node_id for n in nodes]
+
+    return db.nodes().ids(ids).options(subqueryload('*'))
+
+
+def copy_subgraph(destination_db, nodes):
+    """Copy :param:`nodes` and all their edges to :param:`destination_db`
+    database
+
+    """
+
+    # Group nodes by class
+    grouped_nodes = defaultdict(list)
+    for node in nodes:
+        grouped_nodes[node.__class__].append(node)
+
+    # Group edges by class
+    edges = chain((n.get_edges() for n in nodes))
+    grouped_edges = defaultdict(list)
+    for edge in edges:
+        grouped_edges[edge.__class__].append(edge)
+
+    with destination_db.session_scope():
+        # Snapshot nodes
+        for node_type, type_nodes in grouped_nodes.iteritems():
+            upsert_nodes(destination_db, node_type, type_nodes)
+
+        # Snapshot edges
+        for edge_type, type_edges in grouped_edges:
+            upsert_edges(destination_db, edge_type, type_edges)

361554e14f763bc2bdcab9c580ce0d52c2ff5c2a
Joshua Miller <jsmiller@uchicago.edu>
2016-09-23 09:49:32 -0500

    fix(schemas): use sqlalchemy join to ignore fkey misses

--- psqlgraph/copier/schemas.py
+++ psqlgraph/copier/schemas.py
@@ -8,13 +8,13 @@ database.
 
 """
 
-from functools32 import lru_cache
-from sqlalchemy.sql import table as sql_table, select, text
-from sqlalchemy.orm import aliased
-from sqlalchemy import literal, exists
 from copy import deepcopy
+from functools32 import lru_cache
 from psqlgraph import Node, Edge
+from sqlalchemy import literal, exists, MetaData, Table
 from sqlalchemy.dialects import postgresql
+from sqlalchemy.orm import aliased
+from sqlalchemy.sql import table as sql_table, select, text
 
 import json
 
@@ -24,11 +24,11 @@ NODE_UPSERT = (
     # without serialization, built-ins, or locks.  Mode `share` was
     # chosen as the lowest mode that protects against concurrent data
     # changes:
-    "Lock table {dst_table} in share mode;"
+    "Lock table {dst_table} in share mode;  \n"
 
     # select the new values from `source`, which should be a
     # sqlalchemy.orm.Query, that will be comiled into this statement
-    "With src_table ({columns}) as ({source}),"
+    "With src_table ({columns}) as ({source}), "
 
     # This is the CTE that
     #   1. will perform our updates and
@@ -58,19 +58,28 @@ NODE_UPSERT = (
 
 # For comments on what this SQL does, see above, as it is very
 # similar with different conditionals that are specific to edges
-EDGE_UPSERT = """
+EDGE_UPSERT = (
+    # Lock to prevent race condition on upsert
+    """
     Lock table {dst_table} in share mode;
+    """
 
+    # Lock to prevent race condition on join filtering for foreign key
+    # constraints
+    """
+    Lock table {dst_src_node_table} in share mode;
+    Lock table {dst_dst_node_table} in share mode;
+    """
+
+    # Upsert source data
+    """
     With src_table ({columns}) as (
         {source}
-
-        -- the following inner joins filter the given selection to
-        -- include only those edges that satisfy the foreign key
-        -- constraints for `src_id` and `dst_id`
-        inner join {dst_src_node_table} src on {src_table}.src_id = src.node_id
-        inner join {dst_dst_node_table} dst on {src_table}.dst_id = dst.node_id
     ),
+    """
 
+    # Upsert CTE
+    """
     upsert as (
         update {dst_table} dst_table set
             {set_props}
@@ -79,21 +88,29 @@ EDGE_UPSERT = """
               dst_table.dst_id = src_table.dst_id
         returning dst_table.*
     )
+    """
 
+    # Insert any edges that were not updated
+    """
     Insert into {dst_table} ({columns})
     Select {columns} from src_table where not exists (
         select 1 from upsert upsert_cte
         where upsert_cte.src_id = src_table.src_id and
               upsert_cte.dst_id = src_table.dst_id
     )
-"""
-
+    """
+)
 
 
 @lru_cache(maxsize=int(1e4))
 def copy_sql_table(table, schema=None):
-    clone = sql_table(table.name, *[c.copy() for c in table.columns])
-    clone.schema = schema
+    """Creates a clone of a given :param:`table` in given :param:`schema`"""
+
+    metadata = MetaData()
+    metadata.schema = schema
+
+    clone = Table(table.name, metadata, *[c.copy() for c in table.columns])
+
     return clone
 
 
@@ -232,6 +249,12 @@ def copy_edges(source_driver, src_schema, dst_schema, query):
     dst_schema_src_table = node_src_cls.__table__
     dst_schema_dst_table = node_dst_cls.__table__
 
+    # Update the query to inner join on the target schema node tables
+    # to filter edges that would fail foreign key constraints
+    query = (query
+             .join(dst_schema_src_table).reset_joinpoint()
+             .join(dst_schema_dst_table).reset_joinpoint())
+
     upsert = EDGE_UPSERT.format(
         source=unicode(query),
         src_table=full_tablename(src_schema, src_schema_edge_table),
@@ -241,6 +264,8 @@ def copy_edges(source_driver, src_schema, dst_schema, query):
         columns=column_names_str(dst_schema_edge_table),
         set_props=column_setter_str(dst_schema_edge_table)
     )
+
+    print upsert
 
     params = query.statement.compile(dialect=postgresql.dialect()).params
     with source_driver.session_scope() as session:

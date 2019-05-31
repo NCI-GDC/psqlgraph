2db47b0ffb85dba769e0d82d312d1717888ca16f
Maksim An <anmaxvl@gmail.com>
2019-05-30 22:17:07 -0500

    feat(factories): add node factory
    add ability to fill in all props
    
    add logic to create graph from nodes and edges
    
    feat(randomize-subgraph): add support for random graph creation given a node label

--- test/models.py
+++ test/models.py
@@ -1,6 +1,51 @@
 from psqlgraph import Node, Edge, pg_property
 
 
+class FakeDictionary(object):
+    def __init__(self):
+        self.schema = {
+            'test': {
+                'required': ['key1'],
+                'properties': {
+                    'key1': {'type': 'string'},
+                    'key2': {'type': 'string'},
+                    'key3': {'type': 'string'},
+                    'new_key': {'type': 'string'},
+                    'timestamp': {
+                        'oneOf': [
+                            {'type': 'string', 'format': 'date-time'},
+                            {'type': 'integer'},
+                        ]
+                    }
+                },
+                'links': [
+                    {'name': 'tests'},
+                    {'name': 'foos'},
+                ],
+            },
+            'foo': {
+                'properties': {
+                    'bar': {'type': 'string'},
+                    'baz': {'enum': ['allowed_1', 'allowed_2']},
+                    'fobble': {
+                        'type': 'integer',
+                        'minimum': 20,
+                        'maximum': 30,
+                    }
+                },
+                'links': [
+                    {'name': 'foobars'},
+                ],
+            },
+            'foo_bar': {
+                'properties': {
+                    'bar': {'type': 'string'}
+                },
+                'links': [],
+            }
+        }
+
+
 class Edge1(Edge):
 
     __src_class__ = 'Test'
@@ -48,6 +93,8 @@ class TestToFooBarEdge(Edge):
 
 class Test(Node):
 
+    _pg_edges = {}
+
     @pg_property
     def key1(self, value):
         assert isinstance(value, (str, type(None)))
@@ -75,6 +122,8 @@ class Foo(Node):
 
     __label__ = 'foo'
 
+    _pg_edges = {}
+
     @pg_property
     def bar(self, value):
         self._set_property('bar', value)
@@ -93,6 +142,38 @@ class FooBar(Node):
     __label__ = 'foo_bar'
     __nonnull_properties__ = ['bar']
 
+    _pg_edges = {}
+
     @pg_property
     def bar(self, value):
         self._set_property('bar', value)
+
+
+Test._pg_edges.update({
+    'tests': {
+        'backref': '_tests',
+        'type': Test,
+    },
+    'foos': {
+        'backref': 'tests',
+        'type': Foo,
+    }
+})
+
+Foo._pg_edges.update({
+    'foobars': {
+        'backref': 'foos',
+        'type': FooBar,
+    },
+    'tests': {
+        'backref': 'foos',
+        'type': Test,
+    }
+})
+
+FooBar._pg_edges.update({
+    'foos': {
+        'backref': 'foobars',
+        'type': Foo,
+    }
+})
---
+++ test/test_mocks.py
@@ -0,0 +1,193 @@
+import random
+import re
+import uuid
+from collections import defaultdict
+
+import pytest
+
+from psqlgraph import Node
+from psqlgraph.mocks import GraphFactory, NodeFactory
+from models import FakeDictionary, Test, Foo, FooBar
+
+
+STRING_MATCH = '[a-zA-Z0-9]{32}'
+DATE_MATCH = '^[0-9]{4}-[0-9]{2}-[0-9]{2}T00:00:00'
+
+
+class FakeModels(object):
+    def __init__(self):
+        self.Node = Node
+        self.Test = Test
+        self.Foo = Foo
+        self.FooBar = FooBar
+
+
+@pytest.fixture(scope='session')
+def gdcmodels():
+    return FakeModels()
+
+
+@pytest.fixture(scope='session')
+def gdcdictionary():
+    return FakeDictionary()
+
+
+@pytest.fixture
+def node_factory(gdcmodels, gdcdictionary):
+    return NodeFactory(gdcmodels, gdcdictionary.schema)
+
+
+@pytest.fixture
+def patched_randrange(monkeypatch):
+    monkeypatch.setattr(random, 'randrange', lambda *args: 20)
+
+
+def validate_test(node):
+    assert isinstance(node, Test)
+    for key in ['key1', 'key2', 'key3', 'new_key']:
+        assert re.match(STRING_MATCH, getattr(node, key))
+    assert re.match(DATE_MATCH, node.timestamp)
+
+
+def validate_foo(node):
+    assert isinstance(node, Foo)
+    assert isinstance(node.fobble, int)
+    assert 20 <= node.fobble <= 30
+    assert re.match(STRING_MATCH, node.bar)
+    assert node.baz in ['allowed_1', 'allowed_2']
+
+
+def validate_foo_bar(node):
+    assert isinstance(node, FooBar)
+    assert re.match(STRING_MATCH, node.bar)
+
+
+@pytest.mark.parametrize('label, validator', [
+    ('test', validate_test),
+    ('foo', validate_foo),
+    ('foo_bar', validate_foo_bar),
+])
+def test_node_factory_all_props(node_factory, label, validator):
+    node = node_factory.create(label, all_props=True)
+
+    validator(node)
+
+
+def test_node_factory_sets_required_for_test_node(node_factory):
+    test_node = node_factory.create('test')
+    assert re.match(STRING_MATCH, test_node.key1)
+    assert all(
+        getattr(test_node, key) is None
+        for key in ['key2', 'key3', 'new_key', 'timestamp']
+    )
+
+
+def test_node_factory_doesnt_set_any_props(node_factory):
+    foo_node = node_factory.create('foo')
+    assert all(
+        getattr(foo_node, key) is None
+        for key in ['bar', 'baz', 'fobble']
+    )
+
+
+def test_init_graph_factory(gdcmodels, gdcdictionary):
+    _ = GraphFactory(gdcmodels, gdcdictionary)
+
+
+def test_graph_factory_with_nodes_and_edges(gdcmodels, gdcdictionary):
+    gf = GraphFactory(gdcmodels, gdcdictionary)
+
+    foobar_uuids = [str(uuid.uuid4())]
+    foo_uuids = [str(uuid.uuid4()), str(uuid.uuid4())]
+    test_uuids = [str(uuid.uuid4()), str(uuid.uuid4()), str(uuid.uuid4())]
+
+    nodes = [
+        {'label': 'test', 'node_id': test_uuids[0]},
+        {'label': 'test', 'node_id': test_uuids[1]},
+        {'label': 'test', 'node_id': test_uuids[2]},
+        {'label': 'foo', 'node_id': foo_uuids[0]},
+        {'label': 'foo', 'node_id': foo_uuids[1]},
+        {'label': 'foo_bar', 'node_id': foobar_uuids[0]},
+    ]
+
+    edges = [
+        {'src': test_uuids[0], 'dst': test_uuids[1]},       # t0 -> t1
+        {'src': test_uuids[0], 'dst': foo_uuids[0]},        # t0 -> f0
+        {'src': test_uuids[1], 'dst': foo_uuids[1]},        # t1 -> f1
+        {'src': test_uuids[2], 'dst': foo_uuids[1]},        # t2 -> f1
+        {'src': test_uuids[0], 'dst': foobar_uuids[0]},     # invalid edge
+        {'src': foo_uuids[0], 'dst': foobar_uuids[0]},      # f0 -> fb0
+        {'src': foo_uuids[1], 'dst': foobar_uuids[0]},      # f1 -> fb0
+    ]
+
+    created_nodes = gf.create_from_nodes_and_edges(nodes=nodes, edges=edges,
+                                                   unique_key='node_id')
+
+    expected_adjacency = defaultdict(set)
+    for edge_info in edges:
+        if edge_info['dst'] == foobar_uuids[0] and \
+                edge_info['src'] == test_uuids[0]:
+            # This edge shouldn't exist
+            continue
+        expected_adjacency[edge_info['dst']].add(edge_info['src'])
+
+    nodes_map = {}
+    for node in created_nodes:
+        nodes_map[node.node_id] = node
+
+    # make sure that number of nodes is as expected
+    assert len(created_nodes) == len(nodes)
+
+    # make sure the links are correct
+    for dst_id, src_ids in expected_adjacency.items():
+        node = nodes_map[dst_id]
+        assert {edge.src.node_id for edge in node.edges_in} == src_ids
+
+
+def assert_all_node_types_created_once(nodes):
+    # since random.randrange was patched, it's guaranteed that all children
+    # will be visited, so we expect 3 nodes 1 for each type
+    type_counts = defaultdict(int)
+    for n in nodes:
+        type_counts[n.label] += 1
+
+    assert len(nodes) == 3
+    assert len(type_counts) == 3
+    assert {'foo', 'test', 'foo_bar'} == set(type_counts.keys())
+
+
+def test_graph_factory_random_subgraph(gdcmodels, gdcdictionary,
+                                       patched_randrange):
+    gf = GraphFactory(gdcmodels, gdcdictionary)
+
+    nodes = gf.create_random_subgraph('foo_bar')
+
+    assert_all_node_types_created_once(nodes)
+
+
+def test_graph_factory_with_globals(gdcmodels, gdcdictionary,
+                                    patched_randrange):
+    graph_globals = {
+        'properties': {
+            'key1': 'abcdefghijklmnopqrstuvwxyz012345',
+            'baz': 'disallowed',
+            'bar': '012345abcdefghijklmnopqrstuvwxyz',
+        }
+    }
+
+    gf = GraphFactory(gdcmodels, gdcdictionary, graph_globals=graph_globals)
+
+    nodes = gf.create_random_subgraph('foo_bar', all_props=True)
+
+    assert_all_node_types_created_once(nodes)
+
+    prop_counts = defaultdict(int)
+    for n in nodes:
+        for key_val in n.props.items():
+            prop_counts[key_val] += 1
+
+    # 'disallowed' is not in Enum and shouldn't be set
+    assert ('baz', 'disallowed') not in prop_counts
+    # 2 nodes have property 'bar' and should be set
+    assert prop_counts.pop(('bar', '012345abcdefghijklmnopqrstuvwxyz')) == 2
+    assert all([val == 1 for val in prop_counts.values()]), prop_counts

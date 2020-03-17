db6a5a89f38ddf3336c1d4952cab398d4590605f
Jawad Qureshi <qureshi@uchicago.edu>
2020-03-17 11:39:52 -0500

    Adding jenkins integration

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup
 
 setup(
-    version='2.0.0',
+    use_scm_version={
+        'local_scheme': 'dirty-tag',
+        'write_to': 'psqlgraph/_version.py',
+    },
+    setup_requires=['setuptools_scm'],
     name='psqlgraph',
     packages=["psqlgraph"],
     install_requires=[

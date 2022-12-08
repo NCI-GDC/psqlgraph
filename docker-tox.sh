6dd20210777b98c5a7a3d4488aef45c334359acf
Michael Byrnes <byrnesmi@uchicago.edu>
2022-12-08 11:12:16 -0600

    DEV-1584: Fix Jenkins.

--- docker-tox.sh
+++ docker-tox.sh
@@ -1,8 +1,6 @@
-#!/usr/bin/bash
-
-/usr/bin/python3.7 -m pip install pipx --user
+/usr/bin/python3.9 -m pip install pipx --user
 pipx install poetry 
 pipx inject poetry poetry-dynamic-versioning
 pipx install tox
 
-tox -r
+tox -rs

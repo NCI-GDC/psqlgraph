1bee0c35f0fe3692e6656df1474e3d0fbeb43d6e
Qiao Qiao <qiaoq@uchicago.edu>
2023-07-13 14:02:44 -0500

    follow style guide line

--- src/psqlgraph/mocks.py
+++ src/psqlgraph/mocks.py
@@ -5,4 +5,4 @@ from psqlgraph_test_utils import mocks
 logger = logging.getLogger(__name__)
 logger.warning(f"This module {__name__} is depreciated, please import {mocks.__name__}")
 
-from psqlgraph_test_utils.mocks import *
+from psqlgraph_test_utils.mocks import GraphFactory, NodeFactory

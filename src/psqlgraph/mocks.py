501d863f1551b96dc2a5e55c502a308ae0946a70
Qiao Qiao <qiaoq@uchicago.edu>
2023-07-13 08:23:06 -0500

    update logging message

--- src/psqlgraph/mocks.py
+++ src/psqlgraph/mocks.py
@@ -3,6 +3,6 @@ import logging
 from psqlgraph_test_utils import mocks
 
 logger = logging.getLogger(__name__)
-logger.warning(f"This module is depreciated, please import from {mocks.__package__}")
+logger.warning(f"This module {__name__} is depreciated, please import {mocks.__name__}")
 
 from psqlgraph_test_utils.mocks import *

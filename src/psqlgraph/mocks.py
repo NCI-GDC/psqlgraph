73313704e9a9109e86d96dad60736402edc846c5
Qiao Qiao <qiaoq@uchicago.edu>
2023-07-24 16:04:09 -0500

    rename mocks to hydrator

--- src/psqlgraph/mocks.py
+++ src/psqlgraph/mocks.py
@@ -1,8 +1,8 @@
 import logging
 
-from psqlgraph_test_utils import mocks
+from psqlgraph import hydrator
 
 logger = logging.getLogger(__name__)
-logger.warning(f"This module {__name__} is depreciated, please import {mocks.__name__}")
+logger.warning(f"This module {__name__} is depreciated, please import {hydrator.__name__}")
 
-from psqlgraph_test_utils.mocks import GraphFactory, NodeFactory
+from psqlgraph.hydrator import GraphFactory, NodeFactory

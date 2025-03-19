25bdf27f5576eed0f2caa1a83d213feeb0aa8baf
Michael Byrnes <byrnesmi@uchicago.edu>
2025-03-19 12:39:48 -0500

    DEV-3409: Clean up Driver.

--- src/psqlgraph/util.py
+++ src/psqlgraph/util.py
@@ -1,15 +1,9 @@
 import logging
-import random
-import time
 from functools import wraps
 from types import FunctionType
 
-from sqlalchemy.exc import IntegrityError
-
 from psqlgraph.exc import ValidationError
 
-#  PsqlNode modules
-DEFAULT_RETRIES = 0
 logger = logging.getLogger(__name__)
 
 
@@ -70,55 +64,3 @@ def sanitize(properties):
         else:
             raise ValueError(f"Cannot serialize {type(value)} to JSONB property")
     return sanitized
-
-
-def default_backoff(retries, max_retries):
-    """This is the default backoff function used in the case of a retry by
-    and function wrapped with the ``@retryable`` decorator.
-
-    The behavior of the default backoff function is to sleep for a
-    pseudo-random time between 0 and 2 seconds.
-
-    """
-
-    time.sleep(random.random() * (max_retries - retries) / max_retries * 2)
-
-
-def retryable(func):
-    """This wrapper can be used to decorate a function to retry an
-    operation in the case of an SQLalchemy IntegrityError.  This error
-    means that a race-condition has occured and operations that have
-    occured within the session may no longer be valid.
-
-    You can set the number of retries by passing the keyword argument
-    ``max_retries`` to the wrapped function.  It's therefore important
-    that ``max_retries`` is included as a kwarg in the definition of
-    the wrapped function.
-
-    Setting ``max_retries`` to 0 will prevent retries upon failure;
-    wrapped function will execute once.
-
-    Similar to ``max_retries``, the kwarg ``backoff`` is a callback
-    function that allows the user of the library to over-ride the
-    default backoff function in the case of a retry.  See `func
-    default_backoff`
-
-    """
-
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        retries = 0
-        max_retries = kwargs.get("max_retries", DEFAULT_RETRIES)
-        backoff = kwargs.get("backoff", default_backoff)
-        while retries <= max_retries:
-            try:
-                return func(*args, **kwargs)
-            except IntegrityError:
-                logger.debug(f"Race-condition caught? ({retries}/{max_retries} retries)")
-                if retries >= max_retries:
-                    logger.error(f"Unable to execute {func}, max retries exceeded")
-                    raise
-                retries += 1
-                backoff(retries, max_retries)
-
-    return wrapper

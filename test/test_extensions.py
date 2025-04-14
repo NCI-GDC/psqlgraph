a6052d328b4af79f1b0341cf80c7938c95173b62
Michael Byrnes <byrnesmi@uchicago.edu>
2025-04-14 18:30:57 -0500

    DEV-3250: Test for dynamic node distinctness.

--- test/test_extensions.py
+++ test/test_extensions.py
@@ -1,7 +1,21 @@
+import inspect
+
 import pytest
+from sqlalchemy import orm
 
 import psqlgraph
 from psqlgraph import ext
+
+SNode, SEdge = ext.register_base_class("sample")
+SBase = ext.get_orm_base("sample")
+
+TNode, TEdge = ext.register_base_class("test")
+TBase = ext.get_orm_base("test")
+
+globals()["SNode"] = SNode
+
+
+def foo(node: SNode) -> int: ...
 
 
 @pytest.mark.parametrize(
@@ -13,6 +27,38 @@ def test_register_bases(ns):
     node_cls, edge_cls = ext.register_base_class(package_namespace=ns)
     assert issubclass(node_cls, psqlgraph.AbstractNode)
     assert issubclass(edge_cls, psqlgraph.AbstractEdge)
+
+
+def test_base_classes_distinct():
+    def create_node(node_base: type[psqlgraph.AbstractNode]) -> type[psqlgraph.AbstractNode]:
+        class AlignedReads(node_base):
+            __label__ = "aligned_reads"
+            __tablename__ = "node_aligned_reads"
+
+            @psqlgraph.pg_property(float)
+            def height(self, value):
+                self._set_property("height", value)
+
+        # Insure that configuration can be called for both.
+        orm.configure_mappers()
+
+        return AlignedReads
+
+    # Check that the base classes are not the same.
+    assert SNode != TNode
+    assert SEdge != TEdge
+
+    sample = create_node(SNode)
+    test = create_node(TNode)
+
+    # Insure that despite same name classes are not same & that they are not
+    # contaminated with the opposites base class.
+    assert sample.__name__ == test.__name__
+    assert sample != test
+    assert TNode not in inspect.getmro(sample)
+    assert SNode in inspect.getmro(sample)
+    assert SNode not in inspect.getmro(test)
+    assert TNode in inspect.getmro(test)
 
 
 @pytest.mark.parametrize("ns", ["sample", "test", None])
